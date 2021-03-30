7f79e4e498ef5efb0b67132d05561635fbce8ce7
ShihChu <juju0411tw@gmail.com>
2021-03-30 10:56:51 +0800

    Upload hash_spec.rb

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../credit_card'
 require 'minitest/autorun'
 
@@ -25,24 +27,52 @@ describe 'Test hashing requirements' do
   describe 'Test regular hashing' do
     describe 'Check hashes are consistently produced' do
       # TODO: Check that each card produces the same hash if hashed repeatedly
+      it 'should check that each card produces the same hash' do
+        cards.map do |cc|
+          hashed = cc.hash
+          _(hashed).wont_be_nil
+          _(hashed).must_equal cc.hash
+        end
+      end
     end
 
     describe 'Check for unique hashes' do
       # TODO: Check that each card produces a different hash than other cards
+      it 'should check that each different card produces a different hash' do
+        hash_len = cards.map(&:hash).uniq.length
+        _(cards.uniq.length).must_equal hash_len
+      end
     end
   end
 
   describe 'Test cryptographic hashing' do
     describe 'Check hashes are consistently produced' do
       # TODO: Check that each card produces the same hash if hashed repeatedly
+      it 'should check that each card produces the same hash' do
+        cards.map do |cc|
+          hashed = cc.hash_secure
+          _(hashed).wont_be_nil
+          _(hashed).must_equal cc.hash_secure
+        end
+      end
     end
 
     describe 'Check for unique hashes' do
       # TODO: Check that each card produces a different hash than other cards
+      it 'should check that each different card produces a different hash' do
+        hash_len = cards.map(&:hash_secure).uniq.length
+        _(cards.uniq.length).must_equal hash_len
+      end
     end
 
     describe 'Check regular hash not same as cryptographic hash' do
       # TODO: Check that each card's hash is different from its hash_secure
+      it 'should check that each card_hash is different from its hash_secure' do
+        cards.map do |cc|
+          norm_hashed = cc.hash
+          _(norm_hashed).wont_equal cc.hash_secure
+        end
+      end
     end
   end
 end
